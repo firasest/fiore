33da30651b10b507f51c79421058d9e5a3799df9
firasest <jlassifiras6@gmail.com>
2020-01-29 23:05:36 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -12,14 +12,14 @@
                             <div class="collapse navbar-collapse" id="navigation">
                             
                                 <ul id="main-nav" class="nav navbar-nav">
-                                    <li class="active"><a href="index.html">Accueil</a></li>
+                                    <li class="active"><a href="index.php">Accueil</a></li>
                                     
                                     <li class="dropdown">
                                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Présentation <span class="fa fa-angle-down"></span></a>
                                         <ul class="dropdown-menu">
-                                            <li><a href="qui-somme-nous.html">Qui sommes-nous ?</a></li>
+                                            <li><a href="qui-somme-nous.php">Qui sommes-nous ?</a></li>
                                            
-                                            <li><a href="le-saviez-vous.html">Le saviez-vous ?</a></li>
+                                            <li><a href="le-saviez-vous.php">Le saviez-vous ?</a></li>
                                             
                                         </ul>
                                     </li>
@@ -27,13 +27,13 @@
                                     <li class="dropdown">
                                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Nos Fromages<span class="fa fa-angle-down"></span></a>
                                         <ul class="dropdown-menu">
-                                            <li><a href="products.html#filter=*">Tous les produits</a></li>
-                                            <li><a href="products.html#filter=.rape">Râpé</a></li>
-                                            <li><a href="products.html#filter=.pate-presse">Pâte pressé</a></li>
-                                            <li><a href="products.html#filter=.pate-file">Pâte filé</a></li>
-                                            <li><a href="products.html#filter=.pate-molle">Pâte molle</a></li>
-                                            <li><a href="products.html#filter=.pate-fraiche">Pâte fraîche</a></li>
-                                            <li><a href="products.html#filter=.a-base-alimentaire">Préparation alimentaire</a></li>
+                                            <li><a href="products.php#filter=*">Tous les produits</a></li>
+                                            <li><a href="products.php#filter=.rape">Râpé</a></li>
+                                            <li><a href="products.php#filter=.pate-presse">Pâte pressé</a></li>
+                                            <li><a href="products.php#filter=.pate-file">Pâte filé</a></li>
+                                            <li><a href="products.php#filter=.pate-molle">Pâte molle</a></li>
+                                            <li><a href="products.php#filter=.pate-fraiche">Pâte fraîche</a></li>
+                                            <li><a href="products.php#filter=.a-base-alimentaire">Préparation alimentaire</a></li>
                                            
                                             
                                         </ul>
@@ -42,8 +42,8 @@
 
                                    <li  >
                                        <div class="navbar-header" id="logoPc">
-                                            <a class="navbar-brand hidden-xs hidden-sm" href="index.html"><img class="lazy" alt="logo" src="images/logo-Fiore-fr-foncé.png" srcset="images/logo-Fiore-fr-foncé.png 1x, images/logo-Fiore-fr-foncé.png 2x"></a>
-                                            <a class="navbar-brand hidden-lg hidden-md" href="index.html"><img  class="lazy" alt="logo" src="images/logo.png" srcset="images/logo.png 1x, images/logo.png 2x"></a>
+                                            <a class="navbar-brand hidden-xs hidden-sm" href="index.php"><img class="lazy" alt="logo" src="images/logo-Fiore-fr-foncé.png" srcset="images/logo-Fiore-fr-foncé.png 1x, images/logo-Fiore-fr-foncé.png 2x"></a>
+                                            <a class="navbar-brand hidden-lg hidden-md" href="index.php"><img  class="lazy" alt="logo" src="images/logo.png" srcset="images/logo.png 1x, images/logo.png 2x"></a>
                                             <button class="toggle-side-menu navbar-toggle" type="button"><span class="fa fa-ellipsis-v"></span></button>
                                             <button class="navbar-toggle collapsed" data-target="#navigation" data-toggle="collapse" type="button"><span class="fa fa-navicon"></span></button>
                                           
@@ -52,9 +52,9 @@
 
 
 
-                                    <li><a href="recette.html">Recettes</a></li>
+                                    <li><a href="recette.php">Recettes</a></li>
 
-                                    <li><a href="contact.html">Contact</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
 
                                     <!--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"></div>
                                     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"></div>
